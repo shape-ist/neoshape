490e4272a91db9163b90f85bfb08690d3e7cf031
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-10-22 20:09:40 +0300

    progress

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -2,11 +2,18 @@
 	import HeaderBtn from './src/HeaderBtn.svelte';
 </script>
 
-<header class="w-full h-20 flex justify-center items-center">
+<header class="w-full h-20 flex justify-center items-center sticky top-0">
 	<div
-		class="rounded-full	px-20 flex items-center absoulte sticky top-0 h-full object-fit bg-gray-800 drop-shadow-md"
+		class="
+		hidden transition-all duration-500
+		px-12 justify-center items-center absoulte sticky top-0 h-full object-fit bg-gray-50 drop-shadow-sm border-gray-200 border
+		w-full mt-0
+		lg:w-auto lg:rounded-3xl lg:mt-6
+		sm:flex"
 	>
 		<HeaderBtn to="/" title="Home" />
 		<HeaderBtn to="/about" title="About" />
+		<HeaderBtn to="/projects" title="Projects" />
+		<HeaderBtn to="/edu" title="Education" />
 	</div>
 </header>
--- src/lib/header/src/HeaderBtn.svelte
+++ src/lib/header/src/HeaderBtn.svelte
@@ -4,17 +4,24 @@
 	export let title = 'Button';
 </script>
 
-<a
-	class:active={$page.path === to}
-	sveltekit:prefetch
-	class="font-title text-white font-bold pr-3 pl-3 align-middle"
-	href={to}
->
-	{title}
-</a>
+<div class="flex flex-row">
+	<a
+		class:active={$page.path === to}
+		sveltekit:prefetch
+		class="font-title text-gray-600 font-medium px-5 align-middle text-sm"
+		href={to}
+	>
+		{title}
+		{#if $page.path === to}
+			<div
+				class="bg-purple-500 h-1 w-1/3 rounded-full mt-1 m-auto transform -translate-y-1"
+			/>
+		{/if}
+	</a>
+</div>
 
 <style>
 	.active {
-		@apply bg-red-500 text-white rounded-lg px-5 py-1 mx-4;
+		@apply font-bold transform translate-y-1 text-gray-800;
 	}
 </style>
