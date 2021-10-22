da0c0c638374d880471519c63df53884721ab456
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-10-22 15:27:52 +0300

    init project

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,11 +1,12 @@
 <script>
-	import { page } from '$app/stores';
 	import HeaderBtn from './src/HeaderBtn.svelte';
 </script>
 
-<header
-	class="absoulte sticky top-0 w-full flex justify-center items-center h-16 bg-gray-800 drop-shadow-md"
->
-	<HeaderBtn to="/" title="Home" />
-	<HeaderBtn to="/about" title="About" />
+<header class="w-full h-20 flex justify-center items-center">
+	<div
+		class="rounded-full	px-20 flex items-center absoulte sticky top-0 h-full object-fit bg-gray-800 drop-shadow-md"
+	>
+		<HeaderBtn to="/" title="Home" />
+		<HeaderBtn to="/about" title="About" />
+	</div>
 </header>
