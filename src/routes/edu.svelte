a49c2ef353f38919bab95e35b2916581083e7e8b
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-10-24 16:03:46 +0300

    prog

---
+++ src/routes/edu.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>Education at Shape</title>
+</svelte:head>
+
+<h1 class="font-big-title font-medium text-xl">Edu Page</h1>
---
+++ src/routes/projects.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>Shape Projects</title>
+</svelte:head>
+
+<h1 class="font-big-title font-medium text-xl">Project Page</h1>
