d99949f4a2408651fb922acd933db1fc0667cdc8
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-11-02 15:39:29 +0300

    smol comit

--- src/lib/landing/src/LandingFooter.svelte
+++ src/lib/landing/src/LandingFooter.svelte
@@ -4,4 +4,14 @@ bg-red-500
 "
 >
 	<h2> Find us On: </h2>
+	<div>
+		<p>
+			icon
+			<a href="instagramURL"> @shape-ist </a>
+		</p>
+		<p>
+			icon
+			<a href="linkedinURL"> @shape-ist </a>
+		</p>
+	</div>
 </div>
