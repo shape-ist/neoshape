6755631a9de0af3f97d7d8d7d264fd2c647f9a80
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2021-11-28 23:07:20 +0300

    seperation of concerns

--- src/lib/entity/src/colorOnMount.js
+++ src/lib/entity/src/colorOnMount.js
@@ -1,3 +1,82 @@
-export default function colorOnMount() {
+import {
+    getAverageRGB,
+    rgbToHsl,
+    generateCSSAccent,
+    blend,
+    hslToHex,
+    rgbToHex,
+} from './color';
+
+export default function colorOnMount(doc) {
+    let mainElement = doc.getElementsByTagName('main')[0];
+    let mainStyle = window.getComputedStyle(mainElement);
+    let bgColor = mainStyle.getPropertyValue('background-color');
+    bgColor = rgbToHex(
+        ...bgColor
+        .replace('rgb', '')
+        .replace('(', '')
+        .replace(')', '')
+        .split(',')
+        .map(function (item) {
+            return parseInt(item, 10);
+        })
+    );
+
+    const gOpacity = 0.2;
+
+    let el = doc.getElementById('entity-icon');
+    let bgEl = doc.getElementById('bg-grad');
+
+    el.setAttribute('crossOrigin', 'anonymous');
+
+    let rgb = getAverageRGB(el);
+    let hsl = rgbToHsl(...rgb);
     
+    // parse grayscales
+    if (hsl[0] == 0 && parseFloat(hsl[1]) < 10) {
+        // grayscale,
+        // use default purple
+        hsl = [270, 100, 50]
+    } else {
+        // colorful,
+        // increase saturation and lightness
+        hsl[1] = 100;
+        hsl[2] = 30;
+    }
+
+    let hslFormatted = [];
+    hslFormatted.push(hsl[0]);
+    hslFormatted.push(hsl[1]);
+    hslFormatted.push(hsl[2]);
+
+    // entity icon
+    let cssAccent = generateCSSAccent(hsl, gOpacity);
+    let cssAccentHex = hslToHex(...hslFormatted);
+    let cssBgProp = `linear-gradient(${cssAccent}, transparent)`;
+    bgEl.style.display = 'none';
+    bgEl.style.background = cssBgProp;
+    bgEl.style.display = 'block';
+
+    let blendedAccent = blend(bgColor, cssAccentHex, gOpacity);
+    doc
+        .getElementById('theme-meta')
+        .setAttribute('content', blendedAccent);
+
+    var lastKnown = window.scrollY;
+    var scrollThreshold = 120;
+    doc.addEventListener('scroll', function (e) {
+        let y = window.scrollY;
+        if (y > scrollThreshold && lastKnown < scrollThreshold) {
+            doc
+                .getElementById('theme-meta')
+                .setAttribute('content', bgColor);
+            bgEl.style.opacity = 0;
+        } else if (y < scrollThreshold && lastKnown > scrollThreshold) {
+            doc
+                .getElementById('theme-meta')
+                .setAttribute('content', blendedAccent);
+            bgEl.style.opacity = 1;
+        }
+        lastKnown = y;
+    });
 }
